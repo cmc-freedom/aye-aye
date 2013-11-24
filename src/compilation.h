1e92a2ae13574e14aa1e63cd988af77c565155c6
Alexandr Shcherbakov <alex.shcherbakov.as@gmail.com>
2013-11-24 18:06:03 +0400

    add compilation.h

---
+++ src/compilation.h
@@ -0,0 +1,24 @@
+#ifndef COMPILATION_H_INCLUDED
+#define COMPILATION_H_INCLUDED
+
+enum Language {UNDEFINED, GCC};
+
+typedef struct {
+  unsigned index;
+  char * path;
+  Language lang;
+} Compiled_program;
+
+typedef struct {
+  int status;
+  char * message;
+  Compiled_program result;
+} Compilation_report;
+
+compilation_report
+compile_it(const char * inpath, const char * outpath, Language lang);
+
+Language
+lang_from_string(const char * c);
+
+#endif // COMPILATION_H_INCLUDED
