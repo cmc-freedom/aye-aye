6410865fba35b684d293cbff634c7d39bb35d4b4
vslutov <vslutov@yandex.ru>
2013-11-25 00:05:21 +0400

    change name to wide_system

--- src/wide_system.h
+++ src/wide_system.h
@@ -4,13 +4,13 @@
 #include <stdio.h>
 
 int
-system_wide_system (const char * command);
+wide_system_system (const char * command);
 
 int
-system_wide_fopen (const char * filename, const char * mode);
+wide_system_fopen (const char * filename, const char * mode);
 
 FILE *
-system_wide_freopen (const char * filename, const char * mode, FILE * stream );
+wide_system_freopen (const char * filename, const char * mode, FILE * stream );
 
 
 #endif // SYSTEM_WIDE_H
