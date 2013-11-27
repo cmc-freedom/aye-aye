2b55371f8e5338f420bda434b9dfc66d987784d3
vslutov <vslutov@yandex.ru>
2013-11-27 19:42:01 +0400

    Add memory free

--- src/wide_system.c
+++ src/wide_system.c
@@ -21,21 +21,35 @@ _system_depended_path (const char *path)
   #endif
 }
 
+void
+_system_depended_free (const char *path)
+{
+  #if defined(_WIN16) || defined(_WIN32)
+    free(path);
+  #endif
+}
+
 int
 wide_system_system (const char *command)
 {
+  command = _system_depended_path(command);
   return system(_system_depended_path(command));
+  _system_depended_path(command);
 }
 
 FILE *
 wide_system_fopen (const char *filename, const char *mode)
 {
+  filename = _system_depended_path(filename);
   return fopen(_system_depended_path(filename), mode);
+  _system_depended_path(filename);
 }
 
 FILE *
 wide_system_freopen (const char *filename, const char *mode, FILE *stream)
 {
+  filename = _system_depended_path(filename);
   return freopen(_system_depended_path(filename), mode, stream);
+  _system_depended_path(filename);
 }
 
