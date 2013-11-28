d36fdcd426e29ef3cba8b29bf1255f0ec4ca47ff
vslutov <vslutov@yandex.ru>
2013-11-28 21:52:31 +0400

    Fix library connect

--- src/wide_system.h
+++ src/wide_system.h
@@ -12,7 +12,5 @@ wide_system_fopen (const char *filename, const char *mode);
 FILE *
 wide_system_freopen (const char *filename, const char *mode, FILE *stream);
 
-#include "wide_system.c"
-
 #endif // WIDE_SYSTEM_H
 
